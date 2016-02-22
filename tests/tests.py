53616a9b56461bc1282a65d7e67248d011120bad
Peter Inglesby <peter.inglesby@gmail.com>
2016-02-22 23:02:25 +0000

    Prepare tests for addition of MetadataModel

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,22 @@ from django.test import TestCase, override_settings
 from .models import Article, Author
 
 
-BASE_PATH = os.path.join('tests', 'test-pages', 'article')
+def get_path(model_name, filename):
+    if model_name == 'article':
+        model_type = 'pages'
+    else:
+        assert False
+
+    return os.path.join('tests', model_type, model_name, filename)
+
+
+def create_author(**kwargs):
+    attrs = {
+        'name': 'Peter',
+    }
+
+    attrs.update(kwargs)
+    return Author.objects.create(**attrs)
 
 
 def create_article(**kwargs):
@@ -34,13 +49,13 @@ class TestModel(TestCase):
 
 
 class TestDeserialization(TestCase):
-    def deserialize(self, filename):
-        path = os.path.join(BASE_PATH, filename)
+    def deserialize(self, model_name, filename):
+        path = get_path(model_name, filename)
         with open(path, 'rb') as f:
             return next(serializers.deserialize('md', f))
 
     def test_deserialization(self):
-        deserialized_obj = self.deserialize('valid.md')
+        deserialized_obj = self.deserialize('article', 'valid.md')
         obj = deserialized_obj.object
 
         self.assertEqual(obj.key, 'valid')
@@ -49,29 +64,29 @@ class TestDeserialization(TestCase):
         self.assertEqual(obj.title, 'All about Django')
 
     def test_deserialization_with_foreign_key(self):
-        related_obj = Author.objects.create(name='Peter')
+        related_obj = create_author()
 
-        deserialized_obj = self.deserialize('with_foreign_key.md')
+        deserialized_obj = self.deserialize('article', 'with_foreign_key.md')
         obj = deserialized_obj.object
 
         self.assertEqual(obj.author, related_obj)
 
     def test_deserialization_with_invalid_yaml(self):
         with self.assertRaises(serializers.base.DeserializationError):
-            self.deserialize('invalid_yaml.md')
-
-    def test_deserialization_with_missing_content(self):
-        with self.assertRaises(serializers.base.DeserializationError):
-            self.deserialize('missing_content.md')
+            self.deserialize('article', 'invalid_yaml.md')
 
     def test_deserialization_with_invalid_object(self):
         with self.assertRaises(serializers.base.DeserializationError):
-            self.deserialize('invalid_object.md')
+            self.deserialize('article', 'invalid_object.md')
+
+    def test_deserialization_with_missing_content(self):
+        with self.assertRaises(serializers.base.DeserializationError):
+            self.deserialize('article', 'missing_content.md')
 
 
 class TestSerialization(TestCase):
-    def _test_roundtrip(self, filename, **kwargs):
-        path = os.path.join(BASE_PATH, filename)
+    def _test_roundtrip(self, model_name, filename):
+        path = get_path(model_name, filename)
         with open(path, 'rb') as f:
             obj = next(serializers.deserialize('md', f)).object
 
@@ -80,20 +95,20 @@ class TestSerialization(TestCase):
         with open(path) as f:
             expected = f.read()
 
-        actual = serializers.serialize('md', [obj], **kwargs)
+        actual = serializers.serialize('md', [obj], use_natural_foreign_keys=True)
         self.assertEqual(actual, expected)
 
     def test_serialization(self):
-        self._test_roundtrip('valid.md')
+        self._test_roundtrip('article', 'valid.md')
 
     def test_serialization_with_foreign_key(self):
-        related_obj = Author.objects.create(name='Peter')
-        self._test_roundtrip('with_foreign_key.md', use_natural_foreign_keys=True)
+        related_obj = create_author()
+        self._test_roundtrip('article', 'with_foreign_key.md')
 
 
 class TestLoadData(TestCase):
     def test_loaddata(self):
-        path = os.path.join(BASE_PATH, 'valid.md')
+        path = get_path('article', 'valid.md')
         management.call_command('loaddata', path, verbosity=0)
 
         obj = Article.objects.get(key='valid')
@@ -108,10 +123,11 @@ class TestDumpToFile(TestCase):
         obj = create_article(**kwargs)
         obj.dump_to_file()
 
-        with open(os.path.join(BASE_PATH, filename)) as f:
+        path = get_path('article', filename).replace('pages', 'pages.bak')
+        with open(path) as f:
             expected = f.read()
 
-        with open(os.path.join('tests', 'pages', 'article', 'django.md')) as f:
+        with open(get_path('article', 'django.md')) as f:
             actual = f.read()
 
         self.assertEqual(actual, expected)
@@ -120,17 +136,22 @@ class TestDumpToFile(TestCase):
         self._test_dump_to_file('valid.md')
 
     def test_dump_to_file_with_foreign_key(self):
-        related_obj = Author.objects.create(name='Peter')
+        related_obj = create_author()
         self._test_dump_to_file('with_foreign_key.md', author=related_obj)
 
     def setUp(self):
-        try:
-            shutil.rmtree(os.path.join('tests', 'pages', 'article'))
-        except FileNotFoundError:
-            pass
+        shutil.move(
+            os.path.join('tests', 'pages'),
+            os.path.join('tests', 'pages.bak')
+        )
 
     def tearDown(self):
         try:
-            shutil.rmtree(os.path.join('tests', 'pages', 'article'))
+            shutil.rmtree(os.path.join('tests', 'pages'))
         except FileNotFoundError:
             pass
+
+        shutil.move(
+            os.path.join('tests', 'pages.bak'),
+            os.path.join('tests', 'pages')
+        )
