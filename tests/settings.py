03fd22882d7e7dbde492515113e5dfbebfcd3464
Peter Inglesby <peter.inglesby@gmail.com>
2016-03-12 00:30:29 +0000

    Add views etc for test app

--- tests/settings.py
+++ tests/settings.py
@@ -1,15 +1,47 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'markdown_deux',
     'tests',
     'django_pages',
 ]
 
 SECRET_KEY = '?'
 
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
+
+ROOT_URLCONF = 'tests.urls'
+
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # TODO remove the need for this
 SERIALIZATION_MODULES = {
---
+++ tests/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import DetailView, ListView, TemplateView
+
+from .models import Article, Author
+
+class HomeView(TemplateView):
+    template_name = 'tests/home_view.html'
+
+
+class ArticleList(ListView):
+    model = Article
+
+
+class ArticleDetail(DetailView):
+    model = Article
+    slug_field = 'key'
+
+
+class AuthorList(ListView):
+    model = Author
+
+
+class AuthorDetail(DetailView):
+    model = Author
+    slug_field = 'key'
