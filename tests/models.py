5b2cfe9b2ce6ae2e15fe56621a2a886eb34e11e8
Peter Inglesby <peter.inglesby@gmail.com>
2016-03-13 13:55:35 +0000

    Remove unnecessary nulls

--- tests/models.py
+++ tests/models.py
@@ -5,13 +5,13 @@ from django_pages.models import MetadataModel, PageModel
 class Author(MetadataModel):
     name = models.CharField(max_length=255)
     editor = models.ForeignKey('Author', null=True)
-    tags = models.ManyToManyField('Tag', related_name='authors', null=True)
+    tags = models.ManyToManyField('Tag', related_name='authors')
 
 
 class Article(PageModel):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, null=True)
-    tags = models.ManyToManyField('Tag', related_name='articles', null=True)
+    tags = models.ManyToManyField('Tag', related_name='articles')
 
 
 class Tag(MetadataModel):
