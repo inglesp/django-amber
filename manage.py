e392afc62eb3922ef8296e3e79cdd224d6cee0d5
Peter Inglesby <peter.inglesby@gmail.com>
2016-07-17 17:23:36 +0100

    Add src to sys.path in manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.path.append('src')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
     from django.core.management import execute_from_command_line
