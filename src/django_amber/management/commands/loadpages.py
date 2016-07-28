665f4b74c7b39d166b6b7dcfa4acb0308c14dbbc
Peter Inglesby <peter.inglesby@gmail.com>
2016-07-28 22:11:16 +0100

    Ensure database is migrated before loading pages
    I'm not sure I can be bothered to work out how to test this

--- src/django_amber/management/commands/loadpages.py
+++ src/django_amber/management/commands/loadpages.py
@@ -1,6 +1,7 @@
 import glob
 import os
 
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 from ...models import DjangoPagesModel
@@ -8,6 +9,8 @@ from ...serialization_helpers import load_from_file
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        call_command('migrate')
+
         paths = []
 
         for model in DjangoPagesModel.subclasses():
